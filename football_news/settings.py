6089921b061ed1648d65b241e561dab82797fb53
Rayyan Emir Muhammad <rayyan.emir@ui.ac.id>
2025-09-03 15:51:11 +0700

    Complete tutorial 1: Django MVT implementation

--- football_news/settings.py
+++ football_news/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main'
 ]
 
 MIDDLEWARE = [
@@ -60,7 +61,7 @@ ROOT_URLCONF = 'football_news.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
